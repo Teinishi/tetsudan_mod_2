f1cfb0de5fe1af6b20b62423c1fe1fc7676f01d3
Teinishi <79356047+Teinishi@users.noreply.github.com>
2025-07-29 21:00:36 +0900

    蛍光灯型室内灯のdaeエクスポート

--- blender/interior_light.py
+++ blender/interior_light.py
@@ -10,9 +10,24 @@ if module_path not in sys.path:
     sys.path.append(module_path)
 export_utils = importlib.import_module("export_utils")
 
+export_path = os.path.join(dirname, "exported")
+
 # downlight コレクションのオブジェクトをエクスポート
-export_utils.auto_export(
-    "downlight",
-    os.path.join(dirname, "exported"),
-    "m_tns_"
-)
+export_utils.auto_export("downlight", export_path, "m_tns_")
+
+# 蛍光灯型のオブジェクトをエクスポート
+for type_name in [f'type{i}' for i in range(1, 6)]:
+    for suffix in ('_base', '_light'):
+        obj = export_utils.get_object_by_name(f'{type_name}{suffix}')
+        export_utils.export_objects([obj], os.path.join(
+            export_path, f'm_tns_interior_light_{type_name}_5{suffix}.dae'))
+
+        # 長さを変更したバージョンをエクスポート
+        dup_obj = export_utils.duplicate_objects([obj])[0]
+        export_utils.translate_vertices(
+            dup_obj, (0, -0.25, 0), lambda c: c[1] >= 0.5)
+        export_utils.translate_vertices(
+            dup_obj, (0, 0.25, 0), lambda c: c[1] <= -0.5)
+        export_utils.export_objects(
+            [dup_obj], os.path.join(export_path, f'm_tns_interior_light_{type_name}_3{suffix}.dae'))
+        export_utils.delete_objects([dup_obj])
--- blender/scripts/export_utils.py
+++ blender/scripts/export_utils.py
@@ -1,11 +1,16 @@
 import os
 import bpy  # type: ignore
+import bmesh  # type: ignore
 
 
 def collection_set_hide(name, val):
     vlayer = bpy.context.scene.view_layers[0]
     if name in vlayer.layer_collection.children:
         vlayer.layer_collection.children[name].hide_viewport = val
+
+
+def get_object_by_name(name):
+    return bpy.data.objects.get(name)
 
 
 def deselect_all_objects():
@@ -27,6 +32,39 @@ def select_objects(objs, add=False):
         add = True
 
 
+def delete_objects(objs):
+    select_objects(objs)
+    bpy.ops.object.delete()
+
+
+def duplicate_objects(objs):
+    select_objects(objs)
+    bpy.ops.object.duplicate()
+    duplicated = [o for o in bpy.context.selected_objects]
+    deselect_all_objects()
+    return duplicated
+
+
+def translate_vertices(obj, translate, condition=True):
+    if condition == True:
+        def condition(): return True
+
+    bpy.context.view_layer.objects.active = obj
+    bpy.ops.object.mode_set(mode='EDIT')
+    bpy.ops.mesh.select_mode(type='VERT')
+    bpy.ops.mesh.select_all(action='DESELECT')
+    bm = bmesh.from_edit_mesh(bpy.context.object.data)
+    obj = bpy.context.active_object
+
+    for _, v in enumerate(bm.verts):
+        coord = obj.matrix_world @ v.co
+        if condition(coord):
+            v.select = True
+    bpy.ops.transform.translate(value=translate)
+
+    bpy.ops.object.mode_set(mode='OBJECT')
+
+
 def export_objects(objs, filepath):
     select_objects(objs)
     os.makedirs(os.path.dirname(filepath), exist_ok=True)
@@ -39,5 +77,7 @@ def export_objects(objs, filepath):
 
 def auto_export(collection_name, path, prefix):
     for obj in bpy.data.collections[collection_name].objects:
-        export_objects([obj], os.path.join(
-            path, f"{prefix}{obj.name}.dae"))
+        export_objects(
+            [obj],
+            os.path.join(path, f"{prefix}{obj.name}.dae")
+        )
